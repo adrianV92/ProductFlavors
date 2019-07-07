bce87bfc109e50029c3b8d609fdf298cc1cbefd4
Adrian Vargas <vargas.adrian@pucp.pe>
2019-07-07 10:26:54 -0500

    Primer commit

---
+++ app/src/main/java/pe/edu/cibertec/productflavors/MainActivity.java
@@ -0,0 +1,22 @@
+package pe.edu.cibertec.productflavors;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView tvDepartment;
+    private TextView tvUrlService;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tvDepartment=findViewById(R.id.tvDepartment);
+        tvDepartment.setText(Department.getDepartment());
+        tvUrlService=findViewById(R.id.tvUrlService);
+        tvUrlService.setText(BuildConfig.BASE_URL);
+    }
+}
